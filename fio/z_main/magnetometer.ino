2222c50f05b4eab41e0a8aea570fa4c012c3149d
Luke <hangz@Luke-Vaio-Linux.(none)>
2012-04-11 18:43:14 -0400

    addition of magnetometer code + separated diagnostic code

---
+++ fio/z_main/magnetometer.ino
@@ -0,0 +1,20 @@
+#include "constants.h"
+
+void readMagnetometer(int* x, int* y, int* z)
+{
+  // Tell the HMC5883 to begin reading at register 3, X's MSB register
+  i2cWrite(MAGNETOMETER_ADDR, 0x03);
+ 
+  byte array[6];
+  
+  i2cRead(MAGNETOMETER_ADDR, 6, array);
+
+  
+  *x = array[0] << 8;  // X msb
+  *x |= array[1];      // X lsb
+  *z = array[2] << 8;  // Z msb
+  *z |= array[3];      // Z lsb
+  *y = array[4] << 8;  // Y msb
+  *y |= array[5];      // Y lsb  
+}
+
